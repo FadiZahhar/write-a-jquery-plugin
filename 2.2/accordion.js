ebdf314de29dbf39bc77f5efc57707d56cf44c31
Jeremy McPeak <jwmcpeak@gmail.com>
2016-06-01 22:54:29 -0500

    initial commit

---
+++ 2.2/accordion.js
@@ -0,0 +1,21 @@
+(function($) {
+    
+    $.fn.accordion = function() {
+        
+        this.find("h3")
+            .addClass("accordion-header")
+            .next()
+            .addClass("accordion-panel")
+            .slideUp();
+            
+        this.on("click", ".accordion-header", function(e) {
+            var next = $(e.target).next();
+            
+            next.slideToggle();
+        });
+        
+        return this;
+    };
+    
+    
+}(jQuery));
---
+++ 2.3/accordion.js
@@ -0,0 +1,42 @@
+(function($) {
+    
+    $.fn.accordion = function(options) {
+        
+        var settings = $.extend({}, $.fn.accordion.defaults, options);
+        
+        this.find("h3")
+            .addClass("accordion-header")
+            .next()
+            .addClass("accordion-panel")
+            .slideUp();
+            
+        var $this = this;
+            
+        this.on("click", ".accordion-header", function(e) {
+            var next = $(e.target).next();
+            var isActive = next.attr("data-active");
+            
+            if (settings.distinct) {
+                $this.find("[data-active]")
+                    .removeAttr("data-active")
+                    .slideToggle();
+                    
+                if (isActive) {
+                    return;
+                }
+            }
+            
+            next.attr("data-active", true)
+                .slideToggle();
+        });
+        
+        return this;
+    };
+    
+    
+    $.fn.accordion.defaults = {
+      distinct: false
+    };
+    
+    
+}(jQuery));
